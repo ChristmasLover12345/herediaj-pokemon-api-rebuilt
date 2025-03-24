5f017f8d01e8df74bf7adcf1896979c7ddcf7a7d
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-24 16:09:34 -0700

    finished navbar front end

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 
 const Navbar = () => {
+const [dropdown, setDropdown] = useState("hidden")
+
+const DropdownToggle = () => {
+
+    if (dropdown == "hidden")
+    {
+        setDropdown("block")
+    }
+    else
+    {
+        setDropdown("hidden")
+    }
+
+}
+
   return (
-    <div>
-        
+    <div className='w-full h-full col-start-2 flex flex-col md:flex-row justify-center md:justify-between'>
+        {/* Random button */}
+        <button className='bg-[#BF0606] border-2 border-white text-white md:text-[25px] lg:text-[30px] text-center w-[90%] md:w-[30%] self-center md:order-3 h-[40px] md:h-[60px] rounded-[5px] mb-1 md:mx-2'>Random</button>
+        {/* Dropdown container */}
+        <div className='w-[90%] md:w-[30%] h-[40px] mb-1 md:mx-2 md:h-[60px] self-center relative md:order-2 inline-block'>
+            {/* Dropdown toggle */}
+            <button onClick={DropdownToggle} className='w-full h-full md:text-[25px] border-2 border-white lg:text-[30px] bg-[#BF0606] text-center rounded-[5px] text-white'>Favorites</button>
+            {/* Dropdown container 2 */}
+            <div className={`${dropdown}  absolute flex justify-center left-[-5%] z-10 rounded-[5px] border-4 border-red-500 min-h-[200px] min-w-[110%]  max-h-[300px] bg-[#BF0606]`}>
+
+                <div className='border-[4px] rounded-[5px] border-white bg-[#98CB98] min-h-[97%] min-w-[97%]  overflow-y-scroll flex flex-col flex-grow items-center justify-center'>
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <input placeholder="search" type="text" className="bg-white border-2 md:mx-2 md:h-[60px] rounded-[5px] text-center text-[30px] border-[#BF0606] w-[90%] md:w-[30%] h-[40px] self-center md:order-1" />
+
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar";
+
  
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#FF0000] h-screen w-screen grid grid-cols-[5%_90%_5%] lg:grid-cols-[2%_96%_2%] grid-rows-[15%_auto_3%]">
+
+    <Navbar />
 
     </div>
   );
