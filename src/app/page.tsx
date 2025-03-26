2c4a13d0898435387fb8738540285bf050609889
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-26 15:53:25 -0700

    Addeed some more logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,27 @@ import Navbar from "./components/Navbar";
 
  
 export default function Home() {
-
-
- const GetInfo = async (userMon: string ) => {
   const [pmName, setPmName] = useState("");
   const [pmLocation, setPmLocation] = useState("");
   const [pmElement, setPmElement] = useState("");
   const [pmImg, setPmImg] = useState("");
   const [pmAbilities, setPmAbilities] = useState("");
   const [pmMoves, setPmMoves] = useState("");
+  const [normalUrl, setNormalUrl] = useState("");
+  const [shinyUrl, setShinyUrl] = useState("");
+  
 
+  const [currentPokemon, setCurrentPokemon] = useState("");
+
+  const rng = (min: number, max: number) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+  
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+ const GetInfo = async (userMon: string ) => {
+ 
   const pokeInfo = await pokemonFetch(userMon)
 
   if (pokeInfo.id > 649)
@@ -28,8 +39,23 @@ export default function Home() {
   }
   else
   {
-    
+    setCurrentPokemon(pokeInfo.name)
   }
+
+  setNormalUrl(pokeInfo.sprites.front_default)
+  setShinyUrl(pokeInfo.sprites.front_shiny)
+
+  setPmImg(pokeInfo.sprites.front_default)
+  setPmName(pokeInfo.name)
+
+  const locationData = await fetch(pokeInfo.location_area_encounters)
+  const locations = await locationData.json()
+
+  if (locations == null || locations == "")
+  { setPmLocation("N/A") }
+  else
+  { setPmLocation(locations[rng(0, locations.length - 1)].location_area.name) }
+
 
  }
 
